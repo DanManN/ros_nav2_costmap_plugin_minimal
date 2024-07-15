5311afc3adb113f41845dd5660efb05e9c20a842
Daniel Nakhimovich <danielnak@tutanota.com>
2024-07-15 15:51:35 -0400

    first

---
+++ 1_install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo apt install ros-humble-turtlebot3-gazebo ros-humble-robot-localization ros-humble-xacro python3-colcon-common-extensions
---
+++ 4_launch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ros2 launch my_nav2_bringup tb3_simulation_launch.py headless:=False
